573180ec6337803303916f6fa3a95dd3754db70f
Ravi Kumar <ravikumar.off66@gmail.com>
2024-01-22 17:38:53 +0530

    todo_APP_React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+let count = 0;
 function App() {
+  let [todoList, updateTodoList] = useState([
+    {
+      id: 0,
+      inputValue: 'React',
+    },
+  ]);
+  let [todoInput, updateInput] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mt-5 w-50">
+      <h3 className="text-center">Todo App in React</h3>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={todoInput}
+          onChange={(e) => {
+            twowayInput(e.target.value);
+          }}
+        />
+        <button
+          className="btn btn-primary"
+          disabled={!todoInput.trim()}
+          onClick={() => {
+            addList();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      <ul className="list-group mt-3">
+        {todoList.map((todoInput) => {
+          return (
+            <li className="list-group-item" key={todoInput.id}>
+              <p>{todoInput.inputValue}</p>
+              <button className="btn" onClick={() => onClear(todoInput.id)}>
+                ✖️
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
+
+  function onClear(TodoId) {
+    let remove = todoList.filter((todo) => todo.id !== TodoId);
+    updateTodoList(remove);
+  }
+
+  function twowayInput(Value) {
+    updateInput(Value);
+  }
+
+  function addList() {
+    if (todoInput === '') return;
+    let addNewTodo = [
+      ...todoList,
+      {
+        id: ++count,
+        inputValue: todoInput,
+      },
+    ];
+    updateTodoList(addNewTodo);
+    console.log(todoList);
+  }
 }
 
 export default App;
